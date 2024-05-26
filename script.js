faf930a8d82809ccc384f9c2b016b6ab146e2719
Felipe Coimbra <137563995+felipecoimbra123@users.noreply.github.com>
2024-05-26 17:20:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-
+// Faz mudar o texto de onde mostram os cálculos e os resultados.
 function adicionarCaracter (char){
     const display = document.getElementById('display')
     if (display.innerText === '0' && char !== '.') {
@@ -10,10 +9,12 @@ function adicionarCaracter (char){
     }
 }
 
+// Faz o botão de limpar funcionar, trocando o texto que já está e voltando a ser 0
 function limpar (){
     display.innerText = '0'
 }
 
+// Faz mudar o texto de onde mostra o cálculo para mostrar o resultado desse cálculo.
 function mostrarResultado () {
     display.innerText = eval(display.innerText)
 }
